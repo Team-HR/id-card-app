ec12e9de3ddfba5669673609e94d00db1acad261
fjavalencia <franzval123@gmail.com>
2024-04-23 10:26:56 +0800

    wip: work on dialog for sig capture

---
+++ src/components/IdCardFront.vue
@@ -0,0 +1,126 @@
+<template>
+  <div
+    class="q-ma-sm"
+    style="
+      border: 1px solid gray;
+      background-color: aqua;
+      height: 12.75cm;
+      width: 8.1cm;
+      font-size: 7pt;
+      position: relative;
+      background-image: url('/public/images/lgu_id_front.jpg');
+      background-position: center;
+      background-size: contain;
+    "
+  >
+    <div
+      style="
+        width: 116px;
+        height: 116px;
+        background-color: aliceblue;
+        border-radius: 100%;
+        position: relative;
+        top: 162px;
+        left: 97px;
+      "
+    >
+      <img
+        :src="imgSrc"
+        style="width: 116px; margin: auto; border-radius: 100%"
+      />
+    </div>
+
+    <div
+      style="
+        margin-top: 10px;
+        width: 100%;
+        text-align: center;
+        position: relative;
+        top: 173px;
+        left: 21px;
+      "
+    >
+      {{ details.idNumber ? details.idNumber : "_ - ____" }}
+    </div>
+
+    <div
+      style="
+        margin-top: 10px;
+        /* margin-bottom: 0px !important;
+              padding-bottom: 0px !important; */
+        margin-left: 20px;
+        font-size: 32pt;
+        width: 100%;
+        text-align: left;
+        line-height: 0.6;
+        font-weight: 500;
+      "
+    >
+      {{ details.lastName }}
+    </div>
+    <div
+      style="
+        margin-top: 0px !important;
+        padding: 0px !important;
+        margin-left: 20px;
+        font-size: 16pt;
+        width: 100%;
+        text-align: left;
+        font-weight: 500;
+      "
+    >
+      {{ details.firstName }}
+      {{ details.middleName }}
+    </div>
+    <div
+      style="
+        margin-top: 0px !important;
+        padding: 0px !important;
+        margin-left: 20px;
+        font-size: 10pt;
+        width: 100%;
+        text-align: left;
+        font-weight: 500;
+        font-stretch: ultra-expanded;
+      "
+    >
+      {{ details.position }}
+    </div>
+    <div
+      style="
+        margin-top: 0px !important;
+        padding: 0px !important;
+        margin-left: 20px;
+        font-size: 10pt;
+        width: 100%;
+        text-align: left;
+        font-weight: 500;
+      "
+    >
+      {{ details.position_function }}
+    </div>
+  </div>
+</template>
+
+<style>
+.detail {
+  margin-left: 10px;
+}
+</style>
+
+<script setup>
+defineOptions({
+  name: "IdCardFront",
+});
+
+const props = defineProps({
+  details: {
+    type: Object,
+    required: true,
+  },
+  imgSrc: {
+    type: String,
+    required: true,
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,223 @@
+<template>
+  <q-page class="q-pa-md">
+    <div class="row" style="position: inherit">
+      <div class="col" style="">
+        <q-select
+          clearable
+          class="q-mb-md"
+          filled
+          v-model="selected_employee"
+          use-input
+          input-debounce="0"
+          label="Search Employee"
+          :options="selections"
+          @filter="filterFn"
+          behavior="menu"
+        >
+          <template v-slot:no-option>
+            <q-item>
+              <q-item-section class="text-grey"> No results </q-item-section>
+            </q-item>
+          </template>
+        </q-select>
+
+        <q-btn class="q-mb-md" @click="getEmployeeData()" type="button"
+          >Get</q-btn
+        >
+
+        <div class="row">
+          <div class="col">
+            <div style="margin-left: 100px">
+              <vue-avatar
+                :width="200"
+                :height="200"
+                :rotation="rotation"
+                :borderRadius="borderRadius"
+                :scale="scale"
+                ref="vueavatar"
+                @vue-avatar-editor:image-ready="onImageReady"
+              >
+              </vue-avatar>
+              <br />
+              <label>
+                Zoom : {{ scale }}x
+                <br />
+                <input
+                  type="range"
+                  min="1"
+                  max="3"
+                  step="0.02"
+                  v-model="scale"
+                />
+              </label>
+              <br />
+              <button v-on:click="saveClicked">Get image</button>
+              <br />
+            </div>
+          </div>
+          <div class="col">
+            <q-form
+              @submit.prevent="onSubmit"
+              @reset="onReset"
+              class="q-gutter-sm_"
+            >
+              <q-input
+                filled
+                v-model="selected_employee_data.idNumber"
+                label="ID Number *"
+                hint=""
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (val && val.length > 0) || 'Please check the ID number',
+                ]"
+              />
+              <q-input
+                filled
+                v-model="selected_employee_data.lastName"
+                label="Last Name"
+                hint=""
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (val && val.length > 0) ||
+                    '* Last Name should not be empty!',
+                ]"
+              />
+
+              <q-input
+                filled
+                v-model="selected_employee_data.firstName"
+                label="First Name"
+                hint=""
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (val && val.length > 0) ||
+                    '* First Name should not be empty!',
+                ]"
+              />
+
+              <q-input
+                filled
+                v-model="selected_employee_data.middleName"
+                label="Middle Name"
+                hint=""
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (val && val.length > 0) ||
+                    '* First Name should not be empty!',
+                ]"
+              />
+
+              <q-btn type="submit">Save</q-btn>
+            </q-form>
+          </div>
+        </div>
+      </div>
+      <div class="col flex flex-center q-mt-lg" style="position: relative">
+        <!-- <div style="background-color: aqua; height: 8.5cm; width: 5.4cm"></div> -->
+        <IdCardFront :details="selected_employee_data" :imgSrc="imgSrc" />
+        <IdCardBack :details="selected_employee_data" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import VueAvatar from "vue-avatar-editor-improved/src/components/VueAvatar.vue";
+
+import IdCardFront from "components/IdCardFront.vue";
+import IdCardBack from "components/IdCardBack.vue";
+
+defineOptions({
+  name: "IndexPage",
+  components: {
+    VueAvatar: VueAvatar,
+    IdCardFront,
+    IdCardBack,
+  },
+
+  data: function data() {
+    return {
+      selected_employee: null,
+      selected_employee_data: {
+        idNumber: null,
+        lastName: null,
+        firstName: null,
+        middleName: null,
+        extName: null,
+        position: null,
+        position_function: null,
+      },
+      employees: [],
+      selections: this.employees,
+      rotation: 0,
+      scale: 1,
+      borderRadius: 0,
+      imgSrc: "",
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      console.log(this.selected_employee_data);
+    },
+    onReset() {},
+    getEmployeeData() {
+      this.$api
+        .post("http://192.168.50.51:8081/test.php", {
+          getEmployeeData: true,
+          employeeId: this.selected_employee.value,
+        })
+        .then(({ data }) => {
+          console.log(data);
+          this.selected_employee_data = data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    filterFn(val, update) {
+      if (val === "") {
+        update(() => {
+          this.selections = this.employees;
+        });
+        return;
+      }
+
+      update(() => {
+        const needle = val.toLowerCase();
+        this.selections = this.employees.filter(
+          (v) => v.label.toLowerCase().indexOf(needle) > -1
+        );
+      });
+    },
+
+    saveClicked: function saveClicked() {
+      var img = this.$refs.vueavatar.getImageScaled();
+      // this.$refs.image.src = img.toDataURL();
+      this.imgSrc = img.toDataURL();
+    },
+    onImageReady: function onImageReady() {
+      this.scale = 1;
+      this.rotation = 0;
+    },
+  },
+
+  created() {
+    this.$api
+      .post("http://192.168.50.51:8081/test.php", {
+        getEmployeeList: "true",
+      })
+      .then(({ data }) => {
+        console.log(data);
+        this.employees = data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+});
+</script>
