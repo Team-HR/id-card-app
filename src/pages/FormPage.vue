842032361bff35087837b0920ba869e9c9506a9e
fjavalencia <franzval@gmail.com>
2025-06-27 15:27:39 +0800

    done

--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -1,90 +1,8 @@
 <template>
-  <!-- <q-form @submit.prevent="onSubmit" @reset="onReset" class="q-ma-sm q-gutter-sm" style="min-width: 800px;">
-      <q-input dense filled v-model="selected_employee_data.empno" label="ID Number *" />
-      <div class="row">
-        <q-input class="col" dense filled v-model="selected_employee_data.lastName" label="Last Name" />
-        <q-input class="col q-ml-sm" dense filled v-model="selected_employee_data.firstName" label="First Name" />
-      </div>
-      <div class="row">
-        <div class="col">
-          <q-input dense filled v-model="selected_employee_data.middleName" label="Middle Name" />
-        </div>
-        <div class="col q-ml-sm">
-          <q-input dense filled v-model="selected_employee_data.extName" label="Name Extension" />
-        </div>
-      </div>
-
-   <q-input dense filled v-model="selected_employee_data.empno" label="ID Number *" />
-
-      <q-input class="col" dense filled v-model="selected_employee_data.position" label="Position" type="textarea"
-               autogrow @keyup.enter.prevent />
-
-      <AutocompleteInput input-label="Section" v-model="selected_employee_data.section" :suggestions="sectionOptions" />
-      <AutocompleteInput input-label="Department" v-model="selected_employee_data.department"
-                         :suggestions="departmentOptions" />
-
-
-
-
-
-
-      <AutocompleteInput input-label="Barangay" v-model="selected_employee_data.address_res_barangay"
-                         :suggestions="addressOptions.res_barangays" />
-      <div class="row">
-        <AutocompleteInput class="col q-mr-sm" input-label="City/Municipality"
-                           v-model="selected_employee_data.address_res_city" :suggestions="addressOptions.res_citys" />
-
-        <AutocompleteInput class="col q-mr-sm" input-label="Zipcode"
-                           v-model="selected_employee_data.address_res_zip_code"
-                           :suggestions="addressOptions.res_zip_codes" />
-      </div>
-      <AutocompleteInput class="col q-mr-sm" input-label="Province"
-                         v-model="selected_employee_data.address_res_province"
-                         :suggestions="addressOptions.res_provinces" />
-
-
-
-
-
-
-
-
-      <div class="row">
-        <q-select class="col" dense outlined v-model="selected_employee_data.gender" :options="['MALE', 'FEMALE']"
-                  label="Gender" />
-        <q-input class="col q-ml-sm" dense filled v-model="selected_employee_data.birthdate" label="Birthdate"
-                 type="date" />
-      </div>
-
-      <div class="row">
-        <q-input class="col" dense filled v-model="selected_employee_data.blood_type" label="Blood Type" />
-        <q-input class="col q-ml-sm" dense filled v-model="selected_employee_data.contact_number"
-                 label="Contact Number" />
-      </div>
-      <q-input dense filled v-model="selected_employee_data.emergency_name" label="Emergency Contact Person" />
-      <q-input dense filled v-model="selected_employee_data.emergency_number" label="Emergency Contact Number" />
-      <div class="q-mt-sm"></div>
-      <q-btn class="q-mr-sm" type="submit">Save</q-btn>
-      <q-btn label="Download"></q-btn>
-
-    </q-form>
- -->
   <div class="q-pa-md">
     <q-stepper v-model="step" vertical color="primary" animated>
       <q-step :name="1" title="Search Personnel" icon="user" :done="step > 1">
         <q-form @submit.prevent="onSubmit(2)" class="q-gutter-sm">
-          <!-- <div class="row">
-            <q-input class="col-4" filled v-model="selected_employee_data.lastName" label="Last Name" />
-            <q-input class="col-4 q-ml-sm" filled v-model="selected_employee_data.firstName" label="First Name" />
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <q-input filled v-model="selected_employee_data.middleName" label="Middle Name" />
-            </div>
-            <div class="col-2 q-ml-sm">
-              <q-input filled v-model="selected_employee_data.extName" label="Name Extension" />
-            </div>
-          </div> -->
           <q-select clearable class="q-mb-md" filled v-model="selected_employee_input" use-input input-debounce="0"
                     label="Select Employee" :options="selections" option-value="value" option-label="label"
                     @filter="filterFn" @clear="onClearForm()" @update:model-value="getEmployeeData()" behavior="menu">
@@ -94,23 +12,31 @@
               </q-item>
             </template>
           </q-select>
+
+          <div class="q-ml-xl">
+            <pre class="q-ma-none">{{ selected_employee_data.position }}</pre>
+            <pre class="q-ma-none">{{ selected_employee_data.position_function }}</pre>
+            <pre class="q-ma-none">{{ selected_employee_data.department }}</pre>
+          </div>
+
           <q-stepper-navigation>
-            <q-btn type="submit" color="primary" label="Continue" />
+            <q-btn :disable="!selected_employee_input" type="submit" color="primary" label="Continue" />
           </q-stepper-navigation>
         </q-form>
       </q-step>
 
-      <q-step :name="2" title="Employment Information" icon="work_outline" :done="step > 2">
+      <q-step :name="2" title="Personal Information" icon="attribution" :done="step > 2">
         <q-form @submit.prevent="onSubmit(3)" class="q-gutter-sm">
-          <!-- <q-input dense filled v-model="selected_employee_data.empno" label="ID Number *" /> -->
-          <q-input class="col" filled v-model="selected_employee_data.position" label="Position" type="textarea"
-                   autogrow @keyup.enter.prevent />
-
-          <AutocompleteInput input-label="Section" v-model="selected_employee_data.section"
-                             :suggestions="sectionOptions" />
-          <AutocompleteInput input-label="Department" v-model="selected_employee_data.department"
-                             :suggestions="departmentOptions" />
-
+          <!-- gender -->
+          <!-- <q-select class="col" filled :options="['MALE', 'FEMALE']" label="Gender"
+                    v-model="selected_employee_data.gender" /> -->
+          <!-- birthdate -->
+          <q-input class="col" filled label="Birthdate" type="date" v-model="selected_employee_data.birthdate" />
+          <!-- bloodtype -->
+          <q-input class="col" filled label="Bloodtype" type="text" v-model="selected_employee_data.blood_type" />
+          <!-- contact number          n/a -->
+          <q-input class="col" filled label="Contact Number" type="text"
+                   v-model="selected_employee_data.contact_number" />
           <q-stepper-navigation>
             <q-btn type="submit" color="primary" label="Continue" />
             <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" />
@@ -142,18 +68,18 @@
       </q-step>
 
 
-      <q-step :name="4" title="Emergency Contact Information" icon="emergency">
-        <q-form @submit.prevent="onSubmit(4)" class="q-gutter-sm">
+      <q-step :name="4" title="Emergency Contact Information" icon="emergency" :done="step > 4">
+        <q-form @submit.prevent="onSubmit(5)" class="q-gutter-sm">
           <div class="row">
-            <div class="col-4">
+            <div class="col">
               <q-input class="q-mb-sm" filled v-model="selected_employee_data.emergency_name"
                        label="Emergency Contact Person" />
               <q-input filled v-model="selected_employee_data.emergency_number" label="Emergency Contact Number" />
             </div>
           </div>
           <q-stepper-navigation>
-            <q-btn color="primary" label="Finish" />
-            <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+            <q-btn type="submit" color="primary" label="Finish" />
+            <q-btn flat @click="step = 3" color="primary" label="Back" class="q-ml-sm" />
           </q-stepper-navigation>
         </q-form>
       </q-step>
@@ -164,13 +90,14 @@
 <script setup>
 import { useQuasar } from "quasar";
 import AutocompleteInput from "src/components/AutocompleteInput.vue";
-
+// const $q = useQuasar()
 
 defineOptions({
   name: "FormPage",
   components: { AutocompleteInput },
   data: function data() {
     return {
+      $q: useQuasar(),
       step: 1,
       departmentOptions: [],
       sectionOptions: [],
@@ -183,7 +110,16 @@ defineOptions({
         extName: null,
         position: null,
         position_function: null,
-        address: null,
+        gender: null,
+        birthdate: null,
+        blood_type: null,
+        contact_number: null,
+        address_res_barangay: null,
+        address_res_city: null,
+        res_zip_codes: null,
+        res_provinces: null,
+        emergency_name: null,
+        emergency_number: null
       },
       selected_employee_input: null,
       employees: [],
@@ -191,8 +127,48 @@ defineOptions({
     };
   },
   methods: {
+
+    onClearForm() {
+      this.$router.push({ path: `/form` }).then(() => {
+        window.location.reload()
+      })
+    },
+
+    async getEmployeeData() {
+      if (!this.selected_employee_input) return;
+      await this.$api.post(import.meta.env.VITE_API_URL + "/id_card_backend.php", {
+        getEmployeeData: true,
+        employeeId: this.selected_employee_input.value,
+      }).then(({ data }) => {
+        this.selected_employee_data = data;
+        console.log(this.selected_employee_data);
+      })
+    },
+
     onSubmit(step) {
-      this.step = step
+      if (step == 5) {
+        console.log(this.selected_employee_input.value);
+        console.log(this.selected_employee_data);
+        this.$api
+          .post(import.meta.env.VITE_API_URL + "/id_card_backend.php", {
+            saveForm: true,
+            selected_employee_data: this.selected_employee_data
+          })
+          .then(({ data }) => {
+            this.$q.notify({
+              progress: true,
+              message: 'Form Submitted!',
+              icon: 'save',
+              color: 'green',
+              timeout: 1500
+            })
+            setTimeout(() => {
+              this.onClearForm()
+            }, 2000)
+
+          });
+      } else
+        this.step = step
     },
 
     onReset() {
@@ -245,11 +221,6 @@ defineOptions({
         this.employees = data;
         // if employee id in route params exists
         // editor is prepopulated for that employee
-        if (this.id) {
-          const itemByValue = this.employees.find(item => item.value == this.id);
-          this.selected_employee_input = itemByValue
-          this.getEmployeeData();
-        }
       })
       .catch((err) => {
         console.error(err);
